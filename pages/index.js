28f6eb03200a8f5916a0fb0f88d9771aa21e10db
ZarckDev <55784207+ZarckDev@users.noreply.github.com>
2021-08-18 04:23:28 +0200

    state basics - class and functions

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,8 @@
-import React from 'react';
-
-// const ArrowFunction = () => {
-//   return (
-//     <div>
-//       <h1>I am arrow function</h1>
-//     </div>
-//   );
-// };
-const ArrowFunction = (params) => (
-  <div>
-    <h1>I am arrow function</h1>
-  </div>
-);
+import React, { useState } from 'react';
 
 function CompA() {
   return (
     <>
-      <ArrowFunction />
       <h1>CompA</h1>
       <p>Hello CompA</p>
     </>
@@ -25,29 +11,56 @@ function CompA() {
 
 //JSX
 class CompC extends React.Component {
+  constructor() {
+    super(); // get constructor from react component
+    this.state = {
+      value: 10,
+    };
+  }
+
+  //OR
+  // state = {
+  //   value: 10,
+  // };
+
+  changeState(inc) {
+    this.setState({
+      value: inc, // new value
+    });
+  }
+
   render() {
-    // return <h1>CompC</h1>;
-    return React.createElement('h1', null, 'Hello CompC'); // no JSX, directly element creation
+    const { value } = this.state;
+
+    return (
+      <>
+        <h1>CompC</h1>
+        Current Value: <h1>{value}</h1>
+        <button onClick={() => this.changeState(value + 1)}>+</button>
+        <button onClick={() => this.changeState(value - 1)}>-</button>
+      </>
+    );
+    // return React.createElement('h1', null, 'Hello CompC'); // no JSX, directly element creation
   }
 }
 
-// export default function () {
-//   return (
-//     <>
-//       <h1>Hello World</h1>
-//       <CompA />
-//       <CompC />
-//     </>
-//   );
-// }
+function Home() {
+  // useState() returns an array
+  const [value, setValue] = useState(0);
 
-const HomePage = () => {
+  console.log('I am called initially and when state is changed!');
+
+  // const changeValue = (inc) => {
+  //   setValue(value + inc);
+  // };
+
   return (
     <>
-      <h1>Hello World</h1>
-      <CompA />
+      Current Value: <h1>{value}</h1>
+      <button onClick={() => setValue(value + 1)}>+</button>
+      <button onClick={() => setValue(value - 1)}>-</button>
       <CompC />
     </>
   );
-};
-export default HomePage;
+}
+export default Home;
