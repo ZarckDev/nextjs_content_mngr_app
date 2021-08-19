4bea506bf1c72f6cdbd9c3dbf209dafcc6576616
ZarckDev <55784207+ZarckDev@users.noreply.github.com>
2021-08-19 10:45:54 +0200

    getServerSideProps vs getStaticProps for data fetching

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,7 @@ import Newsletter from 'components/Newsletter';
 import ResourceList from 'components/ResourceList';
 import Footer from 'components/Footer';
 
-import { resources } from 'api/data';
-
-function Home() {
+function Home({ resources }) {
   return (
     <Layout>
       {/* only show the first 2 resources */}
@@ -19,4 +17,33 @@ function Home() {
     </Layout>
   );
 }
+
+// is called everytime you will visit the page
+// function is executed on the server
+// data are always fresh
+export async function getServerSideProps() {
+  const resData = await fetch('http://localhost:3000/api/resources');
+  const data = await resData.json();
+
+  return {
+    props: {
+      resources: data,
+    },
+  };
+}
+
+// is called at the build time, and it's called only once
+// build time so when the html file is created !
+//
+// export async function getStaticProps() {
+//   const resData = await fetch('http://localhost:3000/api/resources');
+//   const data = await resData.json();
+
+//   return {
+//     props: {
+//       resources: data,
+//     },
+//   };
+// }
+
 export default Home;
