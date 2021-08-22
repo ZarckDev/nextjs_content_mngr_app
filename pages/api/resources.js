6095414d9d77fbb04e9d89fa27b2ee195167defe
ZarckDev <55784207+ZarckDev@users.noreply.github.com>
2021-08-22 15:20:28 +0200

    New resource Post request

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,6 +1,27 @@
-export default async function (req, res) {
-  const dataRes = await fetch('http://http://localhost:3001/api/resources');
-  const data = await dataRes.json();
+import axios from 'axios';
 
-  res.send(data);
+export default async function (req, res) {
+  if (req.method === 'GET') {
+    const dataRes = await fetch('http://localhost:3001/api/resources');
+    const data = await dataRes.json();
+
+    return res.send(data);
+  }
+
+  if (req.method === 'POST') {
+    const { title, description, link, timeToFinish, priority } = req.body;
+    if (!title || !description || !link || !timeToFinish || !priority) {
+      return res.status(422).send('Data are missing!');
+    }
+
+    try {
+      const axiosRes = await axios.post(
+        'http://localhost:3001/api/resources',
+        req.body
+      );
+      return res.send(axiosRes.data); // message from server (content-manager-app-api)
+    } catch (e) {
+      return res.status(422).send('Data cannot be stored');
+    }
+  }
 }
--- pages/resources/new.js
+++ pages/resources/new.js
@@ -1,5 +1,7 @@
 import Layout from 'components/Layout';
+import axios from 'axios';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const DEFAULT_DATA = {
   title: '',
@@ -11,9 +13,21 @@ const DEFAULT_DATA = {
 
 const ResourceCreate = () => {
   const [form, setForm] = useState(DEFAULT_DATA);
+  const router = useRouter();
 
   const submitForm = () => {
-    alert(JSON.stringify(form));
+    // request from the browser, so provide relative path is ok
+    // fetch('/api/resources', {
+    //   body: JSON.stringify(form),
+    //   headers: { 'Content-Type': 'application/json' },
+    //   method: 'POST',
+    // });
+    axios
+      .post('/api/resources', form)
+      .then((res) => router.push('/')) // redirect to home page
+      .catch(
+        (err) => alert(err?.response?.data) // ? in case of undefined data
+      );
   };
 
   const resetForm = () => setForm(DEFAULT_DATA);
